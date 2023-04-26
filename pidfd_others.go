5324dfbdb18fcf0a0ae7dcac02ebdd1217a4b39e
WGH <wgh@torlan.ru>
2023-04-26 21:06:38 +0300

    Use waitid in WaitContext (#2)
    WaitContext doesn't reap the process (as before) thanks to WNOWAIT flag.
    In the future, it might make sense to customize this behaviour.
    
    Closes #1

--- pidfd_others.go
+++ pidfd_others.go
@@ -3,6 +3,7 @@
 package pidfd
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"os"
@@ -20,6 +21,7 @@ func open(_ int) (*File, error) { return nil, errUnimplemented }
 type conn struct{}
 
 func (*File) sendSignal(_ os.Signal) error { return errUnimplemented }
+func (*File) wait(_ context.Context) error { return errUnimplemented }
 
 func (*conn) Close() error                      { return errUnimplemented }
 func (*conn) SetReadDeadline(_ time.Time) error { return errUnimplemented }
